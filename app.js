b5022bd39cccc6e38c3528f9cce44bbcf1d589b0
misticG <43235039+MisticG@users.noreply.github.com>
2022-11-07 15:09:20 +0100

    logic

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 window.onload = function () {
 
+    //declaring vaiables
     let seconds = 00;
     let tens = 00;
     let appendTens = document.getElementById('tens');
@@ -8,4 +9,45 @@ window.onload = function () {
     let buttonStop = document.getElementById('button-stop');
     let buttonReset = document.getElementById('button-reset');
     let Interval;
+
+    buttonStart.onclick = () => {
+        clearInterval(Interval);
+        Interval = setInterval(startTimer, 10);
+    }
+
+    buttonStop.onclick = () => {
+        clearInterval(Interval);
+    }
+
+    buttonReset.onclick = () => {
+        clearInterval(Interval);
+        tens = '00';
+        seconds = '00';
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+
+    startTimer = () => {
+        tens++;
+
+        if(tens <= 9) {
+            appendTens.innerHTML = '0' + tens;
+        }
+
+        if(tens > 9) {
+            appendTens.innerHTML = tens;
+        }
+
+        if(tens > 99) {
+            console.log('seconds');
+            seconds++;
+            appendSeconds.innerHTML = '0' + seconds;
+            tens = 0;
+            appendTens.innerHTML = '0' + 0;
+        }
+
+        if(seconds > 9) {
+            appendSeconds.innerHTML = seconds;
+        }
+    }
 }
