35212c8e11992fe78c1250a2f0d0445c1657c683
misticG <43235039+MisticG@users.noreply.github.com>
2022-11-07 14:44:28 +0100

    declaring variables

---
+++ app.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+
+    let seconds = 00;
+    let tens = 00;
+    let appendTens = document.getElementById('tens');
+    let appendSeconds = document.getElementById('seconds');
+    let buttonStart = document.getElementById('button-start');
+    let buttonStop = document.getElementById('button-stop');
+    let buttonReset = document.getElementById('button-reset');
+    let Interval;
+}
